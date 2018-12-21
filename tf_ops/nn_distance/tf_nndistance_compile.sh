7c0ccc1d489eb15910a888726384e57734eae100
GitKraken <matengzhou@163.com>
2018-12-21 15:39:52 +0800

    failedToRun

--- tf_ops/nn_distance/tf_nndistance_compile.sh
+++ tf_ops/nn_distance/tf_nndistance_compile.sh
@@ -1,3 +1,2 @@
-/usr/local/cuda-8.0/bin/nvcc tf_nndistance_g.cu -o tf_nndistance_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
-g++ -std=c++11 tf_nndistance.cpp tf_nndistance_g.cu.o -o tf_nndistance_so.so -shared -fPIC -I /usr/local/lib/python2.7/dist-packages/tensorflow/include -I /usr/local/cuda-8.0/include -I /usr/local/lib/python2.7/dist-packages/tensorflow/include/external/nsync/public -lcudart -L /usr/local/cuda-8.0/lib64/ -L/usr/local/lib/python2.7/dist-packages/tensorflow -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+/usr/local/cuda-9.0/bin/nvcc -std=c++11 -c -o tf_nndistance_g.cu.o tf_nndistance_g.cu -I ~/anaconda3/envs/mainenv/lib/python3.6/site-packages/tensorflow/include -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -O2 && g++ -std=c++11 tf_nndistance.cpp tf_nndistance_g.cu.o -o tf_nndistance_so.so -shared -fPIC -I ~/anaconda3/envs/mainenv/lib/python3.6/site-packages/tensorflow/include -L /usr/local/cuda-9.0/lib64 -O2
 
